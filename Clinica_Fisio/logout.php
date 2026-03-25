83bd8fd25054aeb4d5cc06f5ff6a6e9c5cc39d7a
Karina Guadalupe Muñoz Avila <za230120315@zapopan.tecmm.edu.mx>
2026-03-25 02:21:47 -0600

    Organizando archivos

--- Clinica_Fisio/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();    // Limpia todas las variables
-session_destroy();  // Destruye la sesión por completo
-
-// Redirige al inicio (que mostrará el login)
-header("Location: index.php");
-exit;
-?>
